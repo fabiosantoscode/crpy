a0b824343ce90de5e566f07378e52307570c8741
João Jerónimo <joaopintojeronimo@gmail.com>
2014-05-16 15:39:24 +0100

    bump

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='CrowdProcess',
-    version='0.1.3',
+    version='0.1.4',
     packages=['crowdprocess'],
     license='MIT',
     maintainer='João Jerónimo',
